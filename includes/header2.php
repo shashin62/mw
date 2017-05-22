f4f0a9f62fb320d4cb92793b326446f748dbed5f
unknown <milind.dalvi@v2mumpc0490.in.v2solutions.com>
2017-05-22 17:15:23 +0530

    Added missing files

---
+++ includes/header2.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-inverse navbar-fixed-top cnav" role="navigation" style="background-color: #fff;">
+      <div class="container" style="margin-bottom: auto !important">
+        <div class="navbar-header" style="padding-bottom: 10px;">
+          <button type="button" class="navbar-toggle collapsed btn-h1 btn-m" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" style=" border: 0 !important;border-radius: 0;margin-top: 14px;">
+            <span class="fa fa-navicon btn-cl btn-cls" style="font-size: 20px;"></span>
+          </button>
+          <a class="navbar-brand" href="http://www.makewhale.com/index.php">
+          <span id="logo" style="top: 50%;">
+          <img alt="" src="img/mlogo-011.png" class="visible-lg visible-md visible-sm"/>
+          <img alt="" src="img/mlogo-011.png" height="" width="60px" class="visible-xs"/>
+          </span> 
+          <span id="l-text"><img alt="" src="img/ltext-01.png" width="130px"/></span>
+          </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse " style="margin-top: 6px;">
+          <ul class="nav navbar-nav navbar-right" id="nav-menu-ul">
+	      	<li>
+	       		<a href="http://www.makewhale.com/about-us.php" class="header1a active-about">
+	        		About
+	       		</a>
+	      	</li>
+	      	<li>
+	       		<a href="http://www.makewhale.com/how-it-works.php" class="header1a active-how-it-work">
+	        		How It Works
+	       		</a>
+	      	</li>
+	      	<li class="dropdown">
+	       		<a href="http://www.makewhale.com/our-work.php">
+	        		Our Work</a><a href="#" class="active-our-work" role="button" aria-haspopup="true" aria-expanded="false" title="" tmp_title="" style="width: 20px; position: absolute;
+	        		 top: 0; left: 79px;"><span class="caret"></span>
+	       		</a>
+	       		<ul class="dropdown-menu">
+      <li><a href="http://www.makewhale.com/products/personalized-gifts.php">Personalized Gifts</a></li>
+      <li><a href="http://www.makewhale.com/products/corporate-gifts.php">Corporate Gifts</a></li>
+      <li><a href="http://www.makewhale.com/products/unique-jewellery.php">Unique Jewellery</a></li>
+      <li><a href="http://www.makewhale.com/products/event-specific-gifts.php">Event-Specific Gifts</a></li>
+      <li><a href="http://www.makewhale.com/products/occasion-based.php">Occasion-Based</a></li>
+    </ul>
+	      	</li>
+	      	<li>
+	       		<a href="http://www.makewhale.com/submitYourIdea.php" -class="header1a active-sub-idea">
+	        		Submit Your Idea
+	       		</a>
+	      	</li>
+	      	<li>
+	       		<a href="http://www.makewhale.com/contact-us.php" class="header1a active-contact">
+	        		Contact
+	       		</a>
+	      	</li>
+	      </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
---
+++ includes/include_css.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+        
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+
+<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+
+<link rel="stylesheet" href="css/animate.min.css" />
+
+<link rel="stylesheet" href="css/colorbox1.css" />
+
+<link rel="stylesheet" href="css/main.css" />
